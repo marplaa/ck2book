f4d18c15d0c660fd7de908e5ecc0c9552f5e14b8
Unknown <martin.plaas+github@gmail.com>
2020-08-16 09:23:53 +0200

    build

--- src/app/renderer.service.ts
+++ src/app/renderer.service.ts
@@ -27,16 +27,19 @@ interface Image {
 export class RendererService {
 
   imageList: Image[] = [];
-  sourcesList = '\n \\newpage \n \\chapter{Quellen}\n  \\begin{tiny} \n';
+  sourcesList = '\n \\newpage \n \\chapter{Quellen}\n \\begin{multicols}{2}\n \\begin{tiny} \n';
   recipesService: RecipesService;
 
   constructor() {
   }
 
+  /**
+   * Wrapper for renderNode
+   */
   render(node: RecipesNode): RenderedBook {
 
-    const nodes = this.renderNode(node);
-    const content = twoColTemplate.frame.replace('{{content}}', nodes + this.sourcesList + '\n \\end{tiny} \n');
+    const nodes = this.renderNode(node) + this.sourcesList + '\n \\end{tiny}\n \\end{multicols} \n';
+    const content = twoColTemplate.frame.replace('{{content}}', nodes);
     const id = '' + Md5.hashStr(content);
     const images = this.imageList;
     this.imageList = [];
@@ -44,6 +47,10 @@ export class RendererService {
     return {id, content, images};
   }
 
+  /**
+   * Renders a single node with a recursion if the node is a chapter
+   * @param node The node to render
+   */
   renderNode(node: RecipesNode): string {
 
     let item;
@@ -51,18 +58,23 @@ export class RendererService {
     let output = '';
     for (item of node.children) {
 
-      if (item.children) { // if chapter
-        if (item.isBottomChapter) {
+      if (item.children) { // if item is a chapter (only a chapter has children)
+
+        // TITLE
+        if (item.isBottomChapter) { // if item is a bottom chapter use the chapter template without the multicols
           renderedItem = twoColTemplate.chapter.replace('{{title}}', item.title);
         } else {
           renderedItem = twoColTemplate.chapter_w_subchapters.replace('{{title}}', item.title);
         }
+
+        // TEXT
         let text = '';
-        if (item.text !== null) {
+        if (item.text !== null && item.text !== '') {
           text = '\\begin{mytextbox}' + this.htmlToTex(item.text) + '\\end{mytextbox}\n';
         }
         renderedItem = renderedItem.replace('{{text}}', text);
 
+        // BACKGROUND IMAGE
         if (item.options.background === 'IMAGE'){
           renderedItem = renderedItem.replace('{{bg-image}}',
             twoColTemplate.background.replace('{{bg-image}}',
@@ -72,12 +84,15 @@ export class RendererService {
         }
 
         let sizes;
+        // if the recipes use the chapter background image, create a normal and a filtered background image
         if (item.options.recipeBackgrounds === 'CHAPTER') {
           sizes = [{size: twoColTemplate.chapterImageRes, filter: {}},
             {size: twoColTemplate.chapterImageRes, filter: {color: 0.3, brightness: 1.5, blur: 15}}] ;
-        } else {
+        } else { // otherwise the unfiltered image is enough
           sizes = [{size: twoColTemplate.chapterImageRes, filter: {}}];
         }
+
+        // add images to the imageList
         let img: Image;
         // check if image is already in list
         const imageInList = this.imageList.filter(image => image.url === item.image);
@@ -91,7 +106,7 @@ export class RendererService {
           }
         }
 
-
+        // Recursion
         if (item.children.length > 0) {
           output += renderedItem.replace('{{children}}', this.renderNode(item));
         } else {
@@ -103,8 +118,11 @@ export class RendererService {
         renderedItem = twoColTemplate.recipe.replace('{{title}}', item.title);
         renderedItem = renderedItem.replace('{{text}}', this.htmlToTex(item.text));
         renderedItem = renderedItem.replace('{{ingredients}}', this.renderTable(item.ingredients));
+
+        // IMAGE
         if (item.hasImage) {
-          const imageSnippet = '\\begin{center}\n \\includegraphics[width=8cm]{' + Md5.hashStr(item.image) + '-' + twoColTemplate.recipeImageRes + '}\n' + '  \\end{center}\n';
+          const imageSnippet = '\\begin{center}\n \\includegraphics[width=8cm]{'
+            + Md5.hashStr(item.image) + '-' + twoColTemplate.recipeImageRes + '}\n' + '  \\end{center}\n';
           renderedItem = renderedItem.replace('{{image}}', imageSnippet );
 
           const parent = this.recipesService.getParentNodeById(item.id);
@@ -124,6 +142,7 @@ export class RendererService {
           renderedItem = renderedItem.replace('{{bg-image}}', '');
         }
 
+        // add image to the imageList
         if (item.hasImage && this.imageList.filter(img => img.url === item.image).length === 0) {
           const img: Image = {
             url: item.image, sizes: [{size: twoColTemplate.recipeImageRes, filter: {}},
@@ -133,6 +152,7 @@ export class RendererService {
         }
         output += renderedItem;
 
+        // create sources entry if the recipe came from a website
         if (item.url !== '') {
           const chaptersString = this.getChaptersAsString(item);
           this.sourcesList += chaptersString.substring(3, chaptersString.length) + '\\\\ \n \\textit{' + item.url + '} \\\\ \\\\ \n';
@@ -145,6 +165,9 @@ export class RendererService {
 
   }
 
+  /**
+   * Creates a breadcrumbs like String recursively, e.g. " - Chapter - Chapter - Chapter - Recipe"
+   */
   getChaptersAsString(recipe: RecipesNode): string {
     if (recipe.id === '000') {
       return '';
@@ -153,6 +176,9 @@ export class RendererService {
     }
   }
 
+  /**
+   * Replaces paragraph, newline, bold, italic and underline with the corresponding latex instructions
+   */
   htmlToTex(text: string): string {
     // replace <strong>
     text = this.texSave(text);
@@ -206,12 +232,15 @@ export class RendererService {
 
     text = text.replace(/<\/br>/g, ' \\\\\n');
     text = text.replace(/<br>/g, ' \\\\\n');
-    text = text.replace(/%/g, '\\%');
+    // text = text.replace(/%/g, '\\%');
 
     return text;
   }
 
 
+  /**
+   * Escapes special latex characters
+   */
   texSave(text: string): string {
     text = text.replace(/%/g, '\\%');
     // text = text.replace(/⅛/g, '1/8');
@@ -219,6 +248,9 @@ export class RendererService {
     return text;
   }
 
+  /**
+   * Renders the ingredients table
+   */
   renderTable(ingredients: string[]): string {
 
     let table = '\\begin{tabulary}{8cm}{R|L}\n';
